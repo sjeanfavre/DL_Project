49e5a0d77d2679c019c8092045c9730e916aed5d
Sébastien Jeanfavre <sebastien.jeanfavre@epfl.ch>
2021-05-23 11:31:19 +0200

    Linear (not sure), sequential added

--- Project2.py
+++ Project2.py
@@ -1,7 +1,7 @@
-import math
-from torch import empty, set_grad_enabled
+import math, torch
 
-set_grad_enabled(False)
+
+torch.set_grad_enabled(False)
 
 class Module(object):
     
@@ -44,14 +44,37 @@ class Module(object):
         '''
         return []
 
-class Linear(Module):
-    # Fully connected layer module
     
-    def __init__(self, in_size, out_size):
-        self.w = torch.rand(in_size, out_size)
-        self.b = torch.rand(out_size, 1)
-        
+# Fully connected layer module    
 
+class Linear(Module):
+    
+    def __init__(self, in_features, out_features, gamma = 1e-3):
+        self.gamma = gamma
+        self.w = torch.rand(in_features, out_features)
+        self.b = torch.rand(out_features, 1)
+        
+    def forward(self, x):
+        self.x = x    #Saving for backprop
+        return torch.mm(self.w,x) + self.b
+    
+    def backward(self, dl_ds):
+        #Derivative of the loss w.r.t. the parameters
+        self.dl_dw = torch.mm(dl_ds,self.x.T)
+        self.dl_db = dl_ds
+        self.dl_dx = torch.mm(self.w.T,dl_ds)
+        
+        #Parameters update with SGD
+        self.w =- self.gamma*dl_dw
+        self.b =- self.gamma*dl_db
+        
+        return dl_dx
+    
+    def param(self):
+        return [(self.w,self.dl_dw), (self.b,self.dl_db)]
+    
+    
+#Loss function
 
 class LossMSE(Module):
     # LossMSE module
@@ -67,22 +90,12 @@ class LossMSE(Module):
         # Computes the MSE loss gradient w.r.t. v
         
         return 2*(self.v-self.t)/v.numel()    
-    
-    
-        
-def generate_data():
-    # Generates uniformly distributed data points in [0,1]^2, each with a label 0 if outside the disk of center (0.5, 0.5) and radius 1/sqrt(2*pi), and 1 inside
-    
-    train_input = torch.rand(1000, 2)
-    test_input = torch.rand(1000, 2)
-    train_target = ((train_input[:,0]-0.5)**2 + (train_input[:,1]-0.5)**2) <= 1/(2*math.pi)
-    test_target = ((test_input[:,0]-0.5)**2 + (test_input[:,1]-0.5)**2) <= 1/(2*math.pi)
-    
-    return train_input, train_target, test_input, test_target
 
+
+#Activation functions
 
 class ReLU(Module):
-    
+
     def forward(self, s):
         self.s = s
         s[s>0] = 0
@@ -102,3 +115,36 @@ class Tanh(Module):
         d_tanh = 1-((1-math.exp(-2*self.s))/(1+math.exp(-2*self.s)))^2
         dl_ds = dl_dx*d-tanh
         return dl_ds
+
+    
+#Sequential
+
+class sequential:
+    def __init__(structure):
+        self.structure = structure
+        
+    def forward(self, x):
+        for layer in self.structure:
+            x = layer.forward(x)
+        
+    def backward(self, dl_dx):
+        for layer in reversed(self.structure):
+            dl_dx = layer.backward(dl_dx)
+    
+    def param(self):
+        parameter = []
+        for layer in self.structure:
+            for parameter in layer.param():
+                parameters.append(parameter)
+            
+# Generation of data            
+
+def generate_data():
+    # Generates uniformly distributed data points in [0,1]^2, each with a label 0 if outside the disk of center (0.5, 0.5) and radius 1/sqrt(2*pi), and 1 inside
+    
+    train_input = torch.rand(1000, 2)
+    test_input = torch.rand(1000, 2)
+    train_target = ((train_input[:,0]-0.5)**2 + (train_input[:,1]-0.5)**2) <= 1/(2*math.pi)
+    test_target = ((test_input[:,0]-0.5)**2 + (test_input[:,1]-0.5)**2) <= 1/(2*math.pi)
+    
+    return train_input, train_target, test_input, test_target
